667e6a49e22295142efea54f3bf807652f9db240
Michał Graczyk <graczuss@gmail.com>
2016-03-23 12:50:15 +0100

    finalnaProcon

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -3,21 +3,41 @@ package wdsr.exercise2.procon;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class BufferManualImpl implements Buffer {
+	final Lock lock = new ReentrantLock();
+	final Condition notFull = lock.newCondition();
+	final Condition notEmpty = lock.newCondition();
 	
-	List<Order> listOfOrder = Collections.synchronizedList(new LinkedList<>());
+	List<Order> listOfOrder = Collections.synchronizedList(new LinkedList<>()); 
 	
 	public void submitOrder(Order order) throws InterruptedException {
-		listOfOrder.add(order);
+		lock.lock();
+		try{
+			while(listOfOrder.size()==100000){
+				notFull.await();
+			}
+			notEmpty.signal();
+			listOfOrder.add(order);
+		} finally {
+			lock.unlock();
+		}
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		if(listOfOrder.isEmpty()) {
-			return null;
-		} else {
+		lock.lock();
+		try{
+			while(listOfOrder.isEmpty()){
+				notEmpty.await();
+			}
+			notFull.signal();
 			return listOfOrder.remove(0);
+		} finally {
+			lock.unlock();
 		}
 	}
 }
